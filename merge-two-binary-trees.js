8bd1b227c0183dda1580e96fdcea9a082bcaccd1
CLooker <chad.looker@gmail.com>
2019-02-16 18:51:31 -0500

    Remove TreeNode definition as it is not needed

--- merge-two-binary-trees.js
+++ merge-two-binary-trees.js
@@ -1,12 +1,5 @@
 // https://leetcode.com/problems/merge-two-binary-trees/
 
-class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = this.right = null;
-  }
-}
-
 const mergeTrees = (t1, t2) => {
   if (t1 === null) return t2;
   if (t2 === null) return t1;
