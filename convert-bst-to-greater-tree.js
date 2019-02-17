c81d73796873670ae2ddd4695bd3c8c2ad79ba72
CLooker <chad.looker@gmail.com>
2019-02-16 22:43:35 -0500

    Add convert-bst-to-greater-tree solution

---
+++ convert-bst-to-greater-tree.js
@@ -0,0 +1,22 @@
+const convertBST = root => {
+  if (!root) return root;
+
+  class _convertBST {
+    constructor(root) {
+      this.sum = 0;
+      return this.convertBST(root);
+    }
+
+    convertBST(root) {
+      if (!root) return;
+
+      this.convertBST(root.right);
+      this.sum += root.val;
+      root.val = this.sum;
+      this.convertBST(root.left);
+      return root;
+    }
+  }
+
+  return new _convertBST(root);
+};
